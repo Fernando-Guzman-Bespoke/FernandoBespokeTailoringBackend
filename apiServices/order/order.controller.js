a7c910ae81b1e7bb492b56e627158d329d273780
Brandon Sicay <sicayb@gmail.com~>
2023-11-10 19:45:52 -0600

    orders

--- apiServices/order/order.controller.js
+++ apiServices/order/order.controller.js
@@ -8,16 +8,14 @@ const createPedidoController = async (req, res) => {
     const {
         cliente_id, fecha, fecha_entrega, tela_cuerpo_id, hilo_cuerpo_id, boton_cuerpo_id, etiqueta_cuerpo_id,
         tela_combinacion_id, ojales_combinacion_id, hilo_boton_combinacion_id, entretela_combinacion_id,
-        iniciales, boton_en_cuello, boton_lateral, mancuerna_doble, bolsa, costura_p_pluma, combinacion, plaquet, cuello, collar_interno, collar_externo,
-        botonera, bies_de_botonera, orilla_de_plaquet, puno_interno, puno_externo, flecha, botonera_flecha, trabita_de_manga, coderas, notas
+        iniciales, notas
     } = req.body;
 
     try {
         const pedidoId = await createPedido({
             cliente_id, fecha, fecha_entrega, tela_cuerpo_id, hilo_cuerpo_id, boton_cuerpo_id, etiqueta_cuerpo_id,
             tela_combinacion_id, ojales_combinacion_id, hilo_boton_combinacion_id, entretela_combinacion_id,
-            iniciales, boton_en_cuello, boton_lateral, mancuerna_doble, bolsa, costura_p_pluma, combinacion, plaquet, cuello, collar_interno, collar_externo,
-            botonera, bies_de_botonera, orilla_de_plaquet, puno_interno, puno_externo, flecha, botonera_flecha, trabita_de_manga, coderas, notas
+            iniciales,  notas
         });
 
         res.status(200).send({ id: pedidoId });
--- apiServices/order/order.model.js
+++ apiServices/order/order.model.js
@@ -8,12 +8,9 @@ const createPedido = async (data) => {
         const resPedidoInsert = await query(
             `INSERT INTO public.pedidos(
                 cliente_id, fecha, fecha_entrega, tela_cuerpo_id, hilo_cuerpo_id, boton_cuerpo_id, etiqueta_cuerpo_id, tela_combinacion_id, 
-                ojales_combinacion_id, hilo_boton_combinacion_id, entretela_combinacion_id, iniciales, boton_en_cuello, boton_lateral, 
-                mancuerna_doble, bolsa, costura_p_pluma, combinacion, plaquet, cuello, collar_interno, collar_externo, botonera, bies_de_botonera, 
-                orilla_de_plaquet, puno_interno, puno_externo, flecha, botonera_flecha, trabita_de_manga, coderas, notas
+                ojales_combinacion_id, hilo_boton_combinacion_id, entretela_combinacion_id, iniciales, notas
             ) VALUES (
-                $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, 
-                $25, $26, $27, $28, $29, $30, $31, $32
+                $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13
             ) RETURNING id`,
             ...Object.values(data)
         );
